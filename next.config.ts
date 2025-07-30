90f0d09cae45c992b36f3c91c0210c9c86a2de05
eldani13 <ll-luis123@hotmail.com>
2025-07-29 23:12:07 -0500

    first committtttt

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
+    disableStaticImages: true,
     domains: ['simulator-full-movil.vercel.app'], // Permite la optimización de imágenes desde tu dominio
   },
 };
