67cba770b3763ba7e06c0e0144e44d6f061529c0
eldani13 <ll-luis123@hotmail.com>
2025-07-29 23:42:53 -0500

    first committtttttttt

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ import { RiDownloadCloudFill } from "react-icons/ri";
 import { MdReportProblem } from "react-icons/md";
 import domtoimage from "dom-to-image-more";
 
-const logo = '/logo.png'
+const logo = "/logo.png";
 
 export default function Home() {
   const [selectedModel, setSelectedModel] = useState(models[0].name);
@@ -77,22 +77,22 @@ export default function Home() {
 
           {/* Botones */}
           <div className="flex flex-col md:flex-row justify-end items-center gap-4 px-4 mb-10">
-            <div>
+            <div className="w-full md:w-[220px]">
               <button
                 onClick={handleDownload}
-                className="w-full md:w-auto bg-[#0142d0] hover:bg-[#003eaa] text-white font-semibold py-2 px-4 rounded-xl shadow-md flex justify-center items-center gap-2"
+                className="w-full h-[50px] bg-[#0142d0] hover:bg-[#003eaa] text-white font-semibold py-2 px-4 rounded-xl shadow-md flex justify-center items-center gap-2"
               >
                 <RiDownloadCloudFill size={24} />
                 Descargar simulación
               </button>
             </div>
-            <div>
+            <div className="w-full md:w-[220px]">
               <a
                 href="https://wa.me/573015222004?text=Hola,%20presento%20inconvenientes%20con%20el%20simulador%20de%20crédito.%20¿Podrían%20ayudarme?"
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                <button className="w-full md:w-auto bg-[#0142d0] hover:bg-[#003eaa] text-white font-semibold py-2 px-4 rounded-xl shadow-md flex justify-center items-center gap-2">
+                <button className="w-full h-[50px] bg-[#0142d0] hover:bg-[#003eaa] text-white font-semibold py-2 px-4 rounded-xl shadow-md flex justify-center items-center gap-2">
                   <MdReportProblem size={24} />
                   ¿Algún problema?
                 </button>
@@ -123,67 +123,65 @@ export default function Home() {
           </div>
         </section>
 
-        <div
-          id="simulacion-descarga"
-          className="w-[500px] h-auto p-6 fixed overflow-hidden border-none"
-          style={{
-            top: "-9999px", // fuera del viewport
-            left: "-9999px",
-            backgroundColor: "#ffffff",
-            border: "none",
-            boxShadow: "none",
-            outline: "none",
-          }}
-        >
-          <div
-            className="absolute top-0 left-0 w-full h-full rounded-xl border-none"
-            style={{
-              backgroundImage: `url('/images/fondo.jpg')`,
-              backgroundSize: "cover",
-              backgroundPosition: "center",
-              backgroundRepeat: "no-repeat",
-              opacity: 0.2,
-              border: "none",
-              boxShadow: "none",
-              outline: "none",
-            }}
-          ></div>
+   <div
+  id="simulacion-descarga"
+  className="w-fit h-auto p-6 fixed overflow-hidden border-none"
+  style={{
+    top: "-9999px", // fuera del viewport
+    left: "-9999px",
+    backgroundColor: "#ffffff",
+    border: "none",
+    boxShadow: "none",
+    outline: "none",
+  }}
+>
+  <div
+    className="absolute top-0 left-0 w-full h-full rounded-xl border-none"
+    style={{
+      backgroundImage: `url('/images/fondo.jpg')`,
+      backgroundSize: "cover",
+      backgroundPosition: "center",
+      backgroundRepeat: "no-repeat",
+      opacity: 0.2,
+      border: "none",
+      boxShadow: "none",
+      outline: "none",
+    }}
+  ></div>
 
-          <div className="relative z-10 text-center">
-            <img
-              src={logo}
-              alt="Logo"
-              width={100}
-              height={100}
-              style={{
-                margin: "0 auto",
-                marginBottom: "1rem",
-                border: "none",
-                boxShadow: "none",
-                outline: "none",
-              }}
-            />
+  <div className="relative z-10 text-center">
+    <img
+      src={logo}
+      alt="Logo"
+      width={100}
+      height={100}
+      style={{
+        margin: "0 auto",
+        marginBottom: "1rem",
+        border: "none",
+        boxShadow: "none",
+        outline: "none",
+      }}
+    />
 
-            <h2
-              className="text-xl font-bold border-none"
-              style={{ color: "#0142d0" }}
-            >
-              Simulación Full Móvil
-            </h2>
-            <p className="text-black font-semibold border-none mt-4">
-              Modelo: {selectedModel}
-            </p>
-            <p className="text-black font-semibold border-none mt-1">
-              Almacenamiento: {almacenamiento}
-            </p>
-            <p className="text-black font-semibold border-none mt-1">
-              Fallas: {fallas.length > 0 ? fallas.join(", ") : "Ninguna"}
-            </p>
-            <p className="text-black font-bold border-none mt-4 text-lg">
-              Valor estimado: ${finalPrice.toLocaleString("es-CO")} COP
-            </p>
-          </div>
-        </div>
+    <h2 className="text-xl font-bold border-none" style={{ color: "#0142d0" }}>
+      Simulación Full Móvil
+    </h2>
+    <p className="text-black font-semibold border-none mt-4">
+      Modelo: {selectedModel}
+    </p>
+    <p className="text-black font-semibold border-none mt-1">
+      Almacenamiento: {almacenamiento}
+    </p>
+    <p className="text-black font-semibold border-none mt-1">
+      Fallas: {fallas.length > 0 ? fallas.join(", ") : "Ninguna"}
+    </p>
+    <p className="text-black font-bold border-none mt-4 text-lg">
+      Valor estimado: ${finalPrice.toLocaleString("es-CO")} COP
+    </p>
+  </div>
+</div>
+
       </main>
 
       {/* Footer siempre al fondo */}
